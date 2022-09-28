4a4de966e7953da50528f9235f051f2ea2fa86dd
Timothi123 <114060745+Timothi123@users.noreply.github.com>
2022-09-28 13:24:27 +0700

    Add files via upload

---
+++ Rumus mencari kebutuhan kalori.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float Berat, Tinggi, Umur, Kalori;
+	cout<<"Mengetahui kebutuhan kalori pria :"<<endl;
+	
+	cout<<"Masukkan berat :";cin>>Berat;
+	
+	cout<<"Masukkan tinggi :";cin>>Tinggi;
+	
+	cout<<"Masukkan umur :";cin>>Umur;
+	
+	cout<<"Masukkan kalori :";cin>>Kalori;
+	
+	Kalori=66.5+(13.75 * Berat) + (5.003 * Tinggi) - (6.75 * Umur);
+	
+	cout<<"Kalori yang dibutuhkan adalah :"<<Kalori<<"kkal";
+	
+	return 0;
+}
