3a104e27b152c1f5d19d6dacc693e070b1546d33
Timothi123 <114060745+Timothi123@users.noreply.github.com>
2022-09-28 13:28:28 +0700

    Add files via upload

---
+++ RumusMencariLuasPersegiPanjang.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int L,P,Luas;
+	
+	cout<<"Program mencari Luas Persegi Panjang"<<endl;
+	
+	cout<<"Masukkan Panjang: "; cin>>P;
+	
+	cout<<"Masukkan Lebar: "; cin>>L;
+	
+	Luas = P * L;
+	
+	cout<<Luas;
+	return 0;
+	}
