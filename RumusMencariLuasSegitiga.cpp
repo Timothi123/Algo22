459237d3eb8c10fd171c865cc0801da2c2a11ea5
Timothi123 <114060745+Timothi123@users.noreply.github.com>
2022-09-28 13:29:48 +0700

    Add files via upload

---
+++ RumusMencariLuasSegitiga.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int a,t,Luas;
+	
+	cout<<"Program Mencari Luas Segitiga"<<endl;
+	
+	cout<<"Masukkan Alas: "; cin>>a;
+	
+	cout<<"Masukkan tinggi: "; cin >> t;
+	
+	Luas = a * t /2;
+	
+	cout<<Luas;
+	
+	return 0;
+}
